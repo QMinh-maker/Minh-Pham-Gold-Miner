5459c8076efad72016866dad52016221da57f18f
Minh <qminh2702@gmail.com>
2025-08-30 16:12:55 +0700

    afde

---
+++ Assets/_asset/Scripts/HookMovement.cs
@@ -0,0 +1,110 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HookMovement : MonoBehaviour
+{
+    public float min_Z = -55f, max_Z = 55f;
+    public float rotate_speed = 5f;
+
+    private float rotate_angle;
+    private bool rotate_right;
+    private bool canRotate;
+
+    public float move_speed = 3f;
+    private float initial_move_speed;
+
+    public float min_Y = -2.5f;
+    private float initial_Y;
+
+    private bool moveDown;
+
+    void Start()
+    {
+        initial_Y = transform.position.y;
+        initial_move_speed = move_speed;
+
+        canRotate = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Rotate();
+        GetInput();
+        MoveRope();
+    }
+
+
+    void Rotate ()
+    {
+        if (!canRotate)
+            return;
+        if (rotate_right)
+        {
+            rotate_angle += rotate_speed * Time.deltaTime;
+        }
+        else
+        {
+            rotate_angle -= rotate_speed * Time.deltaTime;
+        }
+        transform.rotation = Quaternion.AngleAxis(rotate_angle, Vector3.forward);
+
+        if (rotate_angle >= max_Z)
+        {
+            rotate_right = false;
+        }
+        else if (rotate_angle <= min_Z)
+        {
+            rotate_right = true;
+        }
+
+    }
+
+    void GetInput()
+    {
+        if (Input.GetMouseButtonDown(0)) { 
+            if (canRotate)
+            {
+                canRotate = false;
+                moveDown = true;
+            }
+        
+        }
+    }
+
+
+    void MoveRope()
+    {
+        if (canRotate)
+            return;
+        if (!canRotate)
+        {
+            Vector3 temp = transform.position;
+            if (moveDown)
+            {
+                temp -= transform.up * Time.deltaTime * move_speed;
+            }
+            else
+            {
+                temp += transform.up * Time.deltaTime * move_speed;
+            }
+            transform.position = temp;
+
+            if (temp.y <= min_Y)
+            {
+                moveDown = false;
+            }
+            if (temp.y >= initial_Y)
+            {
+                canRotate = true;
+
+                move_speed = initial_move_speed;
+            }
+
+        }
+
+
+    }
+
+}
---
+++ Assets/_asset/Scripts/RopeRenderer.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class RopeRenderer : MonoBehaviour
+{
+    private LineRenderer lineRenderer;
+
+    [SerializeField] private Transform startPosition;
+
+    private float Line_width = 0.05f;
+
+    private void Awake()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+
+        lineRenderer.startWidth = Line_width;
+
+        lineRenderer.enabled = false;
+    }
+    void Start()
+    {
+        
+    }
+
+    public void RenderLine(Vector3 endPosition, bool enableRenderer)
+    {
+        if (enableRenderer)
+        {
+            if (!lineRenderer.enabled)
+            {
+                lineRenderer.enabled = true;
+            }
+            lineRenderer.positionCount = 2;
+        }
+        else
+        {
+            lineRenderer.positionCount = 0;
+            if (lineRenderer.enabled)
+            {
+                lineRenderer.enabled = false;
+            }
+        }
+        if (lineRenderer.enabled)
+        {
+            Vector3 temp = startPosition.position;
+            temp.z = 0f;
+
+            startPosition.position = temp;
+
+            temp = endPosition;
+            temp.z = 0f;
+            endPosition = temp;
+            lineRenderer.SetPosition(0,startPosition.position);
+            lineRenderer.SetPosition(1, endPosition);
+        }
+
+
+
+    }
+}
