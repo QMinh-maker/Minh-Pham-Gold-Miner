1700fae3a6a03dd9c8fd2c1565a9d6bb7c1cafd7
Minh <qminh2702@gmail.com>
2025-09-15 21:30:56 +0700

    ỷtdj

---
+++ Assets/FirstLoading.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FirstLoading : MonoBehaviour
+{
+    [SerializeField] private float delay = 3f;   // Thời gian chờ (3 giây)
+    [SerializeField] private string nextSceneName; // Index của scene kế tiếp (hoặc thay bằng tên)
+    void Start()
+    {
+        // Gọi hàm LoadNextScene sau "delay" giây
+        Invoke(nameof(LoadNextScene), delay);
+    }
+
+    void LoadNextScene()
+    {
+        if (!string.IsNullOrEmpty(nextSceneName))
+        {
+            SceneManager.LoadScene(nextSceneName);
+        }
+        else
+        {
+            Debug.LogWarning("Chưa gán tên Scene kế tiếp trong Inspector!");
+        }
+    }
+}
