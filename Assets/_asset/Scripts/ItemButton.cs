53719346fc0e64309418f0c783e124e50bea0e45
QMinh-maker <qminh2702@gmail.com>
2025-09-18 21:21:20 +0700

    sdfgjh

---
+++ Assets/_asset/Scripts/ItemButton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemButton : MonoBehaviour
+{
+    public GameObject[] objects; // danh sách object có sẵn
+    private GameObject currentObject;
+
+    public void ShowObject(int index)
+    {
+        // Ẩn object cũ
+        if (currentObject != null)
+        {
+            currentObject.SetActive(false);
+        }
+
+        // Hiện object mới
+        if (index >= 0 && index < objects.Length)
+        {
+            currentObject = objects[index];
+            currentObject.SetActive(true);
+        }
+    }
+}
---
+++ Assets/_asset/Scripts/ItemPrice.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ItemPrice : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI PriceText;
+    private int price;
+    private bool isInitialized = false;
+    void Start()
+    {
+        if (!isInitialized)
+        {
+            price = Random.Range(1, 801); // random một lần
+            isInitialized = true;
+        }
+
+        PriceText.text = "$" + price;
+    }
+}
