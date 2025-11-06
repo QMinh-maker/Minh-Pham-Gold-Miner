0bbef8f731fd5261753f72e3dd8d4be26524ae43
Minh <qminh2702@gmail.com>
2025-11-06 20:14:16 +0700

    l'o

---
+++ Assets/_asset/Scripts/TempoPause.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempoPause : MonoBehaviour
+{
+    public void PauseForOneSecond()
+    {
+        StartCoroutine(PauseCoroutine());
+    }
+
+    private IEnumerator PauseCoroutine()
+    {
+        // Dừng game
+        Time.timeScale = 0f;
+        Debug.Log("⏸ Game paused!");
+
+        // Chờ 1 giây theo thời gian thực (không bị ảnh hưởng bởi timeScale)
+        yield return new WaitForSecondsRealtime(1f);
+
+        // Chạy lại
+        Time.timeScale = 1f;
+        Debug.Log("▶ Game resumed!");
+    }
+}
